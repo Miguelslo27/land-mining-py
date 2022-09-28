4041e98afa9037369768ed67605d470db72708a8
Miguel Sosa <miguel.sosa@halopowered.com>
2022-09-28 17:00:19 -0300

    Clean things up

--- main.py
+++ main.py
@@ -1,52 +1,9 @@
 import time
-# import gspread
 import pandas as pd
-# from pyquery import PyQuery
-# from oauth2client.service_account import ServiceAccountCredentials
 from imc import get_debt
 from gspread_utils import get_spread_content
 
 gst = time.time()
-
-# # defining the scope of the application
-# scope_app = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-# st = time.time()
-# print('==> Get service account credentials...')
-# #credentials to the account
-# cred = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope_app)
-# et = time.time()
-# elapsed_time = et - st
-# print('<== Service account credentials ready in', elapsed_time, 'seconds!')
-
-# st = time.time()
-# print('==> Get Google Spreadsheet client...')
-# # authorize the clientsheet 
-# client = gspread.authorize(cred)
-# et = time.time()
-# elapsed_time = et - st
-# print('<== Google Spreadsheet client ready in', elapsed_time, 'seconds!')
-
-# st = time.time()
-# print('==> Get spreadsheet "Terrenos la costa"...')
-# # get the sample of the Spreadsheet
-# sheet = client.open('Terrenos la costa')
-# et = time.time()
-# elapsed_time = et - st
-# print('<== Spreadsheet "Terrenos la costa" ready in', elapsed_time, 'seconds!')
-
-# st = time.time()
-# print('==> Get first worksheet...')
-# # get the first sheet of the Spreadsheet
-# sheet_instance = sheet.get_worksheet(0)
-# et = time.time()
-# elapsed_time = et - st
-# print('<== Worksheet ready in', elapsed_time, 'seconds!')
-
-# print(inspect.getmembers(sheet_instance, predicate = inspect.ismethod))
-# print(*inspect.getmembers(sheet_instance, predicate = inspect.ismethod), sep = "\n")
-
-# sheet = get_spreadsheet('Terrenos la costa')
 
 st = time.time()
 print('==> Get all sheet records...')
@@ -55,9 +12,6 @@ records = get_spread_content('Terrenos la costa')
 et = time.time()
 elapsed_time = et - st
 print('<== Sheet records ready in', elapsed_time, 'seconds!')
-
-# view the data with no format
-# print(*records, sep="\n")
 
 st = time.time()
 print('==> Convert records to data frame...')
@@ -78,36 +32,3 @@ print('|============================================|')
 
 # view the top records
 print(records_df.head())
-# print(records_df.iloc(0)['Código Municipal'])
-
-# print(getDebt(1)) # NOT FOUND
-# print(getDebt(106995)) # NO DEBT
-# print(getDebt(107000)) # IN DEBT
-
-# Create a file for the result
-# with open('examples/with-debt.html', 'w') as f:
-#     f.write(response.text)
-
-# Parse the result
-# obj = PyQuery(getDebt(1))
-# print('Result')
-# print(obj('span#span__MENSAJERETORNO').text())
-
-# Parse examples
-# NOT_FOUND
-# not_found = open("examples/not-found.html", 'r').read()
-# not_found_obj = PyQuery(not_found)
-# print('Result')
-# print(not_found_obj('span#span__MENSAJERETORNO').text())
-
-# NO_DEBT
-# with_no_debt = open("examples/with-no-debt.html", 'r').read()
-# with_no_debt_obj = PyQuery(with_no_debt)
-# print('Result')
-# print(with_no_debt_obj('span#span__MENSAJERETORNO').text())
-
-# DEBT
-# with_debt = open("examples/with-debt.html", 'r').read()
-# with_debt_obj = PyQuery(with_debt)
-# print('Result')
-# print(float(with_debt_obj('span#span__MONTOFINAL_0001').text().replace('.', '').replace(',','.'), 2))
