922c8e31022e65fe73273d490fad9e301ca07163
Miguel Sosa <miguel.sosa@halopowered.com>
2022-09-28 12:58:16 -0300

    Improve time tracking

--- main.py
+++ main.py
@@ -5,54 +5,73 @@ from pyquery import PyQuery
 from oauth2client.service_account import ServiceAccountCredentials
 from imc import getDebt
 
-# get the start time
-st = time.time()
+gst = time.time()
 
 # defining the scope of the application
 scope_app = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-print('Get service account credentials...')
+st = time.time()
+print('==> Get service account credentials...')
 #credentials to the account
 cred = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope_app)
-print('Service account credentials ready!')
+et = time.time()
+elapsed_time = et - st
+print('<== Service account credentials ready in', elapsed_time, 'seconds!')
 
-print('Get Google Spreadsheet client...')
+st = time.time()
+print('==> Get Google Spreadsheet client...')
 # authorize the clientsheet 
 client = gspread.authorize(cred)
-print('Google Spreadsheet client ready!')
+et = time.time()
+elapsed_time = et - st
+print('<== Google Spreadsheet client ready in', elapsed_time, 'seconds!')
 
-print('Get spreadsheet "Terrenos la costa"...')
+st = time.time()
+print('==> Get spreadsheet "Terrenos la costa"...')
 # get the sample of the Spreadsheet
 sheet = client.open('Terrenos la costa')
-print('Spreadsheet "Terrenos la costa" ready')
+et = time.time()
+elapsed_time = et - st
+print('<== Spreadsheet "Terrenos la costa" ready in', elapsed_time, 'seconds!')
 
-print('Get first worksheet...')
+st = time.time()
+print('==> Get first worksheet...')
 # get the first sheet of the Spreadsheet
 sheet_instance = sheet.get_worksheet(0)
-print('Worksheet ready!')
+et = time.time()
+elapsed_time = et - st
+print('<== Worksheet ready in', elapsed_time, 'seconds!')
 
 # print(inspect.getmembers(sheet_instance, predicate = inspect.ismethod))
 # print(*inspect.getmembers(sheet_instance, predicate = inspect.ismethod), sep = "\n")
 
-print('Get all sheet records...')
+st = time.time()
+print('==> Get all sheet records...')
 # get all the records of the data
 records = sheet_instance.get_all_records()
-print('Sheet records ready!')
+et = time.time()
+elapsed_time = et - st
+print('<== Sheet records ready in', elapsed_time, 'seconds!')
 
 # view the data with no format
 # print(*records, sep="\n")
 
-print('Convert records to data frame...')
+st = time.time()
+print('==> Convert records to data frame...')
  # convert the json to dataframe with pandas
 records_df = pd.DataFrame.from_dict(records)
-print('Data frame ready!')
+et = time.time()
+elapsed_time = et - st
+print('<== Data frame ready in', elapsed_time, 'seconds!')
 
 # get the end time
-et = time.time()
+get = time.time()
 
 # get the execution time
-elapsed_time = et - st
-print('Execution time:', elapsed_time, 'seconds')
+global_elapsed_time = get - gst
+print('|============================================|')
+print('| Execution time:', global_elapsed_time, 'seconds |')
+print('|============================================|')
 
 # view the top records
 print(records_df.head())
