bb38b880d7c317ae901e27504e8fb5f818931829
Miguel Sosa <miguel.sosa@halopowered.com>
2022-10-01 13:50:38 -0300

    Fix index and improve error handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import pandas as pd
 import constants
@@ -46,39 +47,51 @@ def get_invoice_data(_id, year):
 
 def process_all():
   for i, row in records_df.iterrows():
-    print('========================================================================')
-    print('========================================================================')
     print(get_debt(row['Código Municipal']))
     debt_status = get_debt(row['Código Municipal'])
 
-    if debt_status['status'] == constants.ERROR:
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'C{i + 2}', constants.ERROR)
-    else:
-      print('----------------')
-      print('|     ' + str(debt_status['since']) + '     |')
-      print('----------------')
-      
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'C{i + 2}', debt_status['debt'])
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'D{i + 2}', str(debt_status['since']))
-      invoice_data = get_invoice_data(row['Código Municipal'], debt_status['since'])
+    try:
+      if debt_status['status'] == constants.ERROR:
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'C{i + 2}', constants.ERROR)
+      else:
+        print('----------------')
+        print('|     ' + str(debt_status['since']) + '     |')
+        print('----------------')
+        
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'C{i + 2}', debt_status['debt'])
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'D{i + 2}', str(debt_status['since']))
+        invoice_data = get_invoice_data(row['Código Municipal'], debt_status['since'])
 
-    if debt_status['status'] == constants.IN_DEBT:
-      invoice_data = get_invoice_data(row['Código Municipal'], debt_status['since'])
+      if debt_status['status'] == constants.IN_DEBT:
+        invoice_data = get_invoice_data(row['Código Municipal'], debt_status['since'])
 
-    print('INVOICE DATA', invoice_data)
+      print('INVOICE DATA', invoice_data)
 
-    if len(invoice_data) > 0:
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'A{i + 2}', invoice_data[0][0])
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'E{i + 2}', invoice_data[0][1])
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'F{i + 2}', invoice_data[0][2])
-      update_spread_content(LANDS_SPREADSHEET_NAME, f'G{i + 2}', invoice_data[0][3])
+      if len(invoice_data) > 0:
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'A{i + 2}', invoice_data[0][0])
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'E{i + 2}', invoice_data[0][1])
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'F{i + 2}', invoice_data[0][2])
+        update_spread_content(LANDS_SPREADSHEET_NAME, f'G{i + 2}', invoice_data[0][3])
+    except:
+      print('∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆')
+      print(sys.exc_info()[0])
+      print('∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆')
+      time.sleep(30)
 
     print('========================================================================')
     print('========================================================================')
+
+    tracking_path = f'last_land_id.txt'
+    open(tracking_path, 'wb').write(row['Código Municipal'])
 
 process_all()
 
 # Test with one code if needed
-# print(get_debt(106996))
-# print(get_invoice_id(106996, 2021))
-# print(request_invoice_copy(106996, 2021, get_invoice_id(106996, 2021))[0][0])
+# print('========================================================================')
+# print('========================================================================')
+# print(get_debt(106999))
+# print(get_invoice_id(106999, 2021))
+# print(request_invoice_copy(106999, 2021, get_invoice_id(106999, 2022)))
+# print('========================================================================')
+# print('========================================================================')
+# print(request_invoice_copy(106999, 2021, get_invoice_id(106999, 2021))[0][0])
